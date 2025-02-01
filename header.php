ff262e83e88f8b4130c311372e23c226b2a6445f
freezenovagames <yhhjsajjsjsj@gmail.com>
2025-02-01 09:45:11 +0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,52 @@
+ <!-- Header Area Start -->
+        <nav class="fag-header navbar navbar-expand-lg">
+        <div class="container">
+           
+            <a class="navbar-brand" href="/index.html"><img src="/assets/img/logo.png" alt="freezenova games" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="menu-toggle"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                <ul class="header_menu  mr-auto">
+                    <li class="nav-item active">
+                        <a href="/index.html" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                            <a href="/action/index.html" class="nav-link">Action</a>
+                    </li>
+                    <li class="nav-item">
+                            <a href="/adventure/index.html" class="nav-link">Adventure</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/bike/index.html" class="nav-link">Bike</a>
+                    </li>        
+                    <li class="nav-item">
+                            <a href="/car/index.html" class="nav-link">Car</a>
+                    </li>        
+                    <li class="nav-item">
+                        <a href="/idle/index.html" class="nav-link">Idle</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/shooting/index.html" class="nav-link">Shooting</a>
+                    </li>
+                   
+			
+			 <li class="nav-item">
+                        <a href="/classroom6x/index.html" class="nav-link">Classroom 6x</a>
+                    </li>
+			<li class="nav-item">
+                        <a href="/game/1v1-lol.html" class="nav-link">1v1.lol Unblocked</a>
+                    </li>
+                </ul>
+
+
+
+
+
+		    
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- Header Area End -->
